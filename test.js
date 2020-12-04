c5452529c2b90497568d01dc1c34cb5cd1229299
Aram Cho <aramcho@Aramui-MacBookPro.local>
2020-12-04 13:47:56 +0900

    this is git test

---
+++ test.js
@@ -0,0 +1,14 @@
+function evenNumber(numbers) {
+    for (var i=0; i<50; i++) {
+        if (numbers[i] === 0) {
+                console.log(numbers[i] +  " is even");
+        }
+        else if (numbers[i] % 2 === 0) {
+                console.log(numbers[i] + " is even");   
+        }
+        else {
+                console.log(numbers[i] + " is odd");
+        }
+}
+}
+
